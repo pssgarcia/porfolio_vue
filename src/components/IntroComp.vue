f2413374b5061c83b57bc1f134b23699c7bbf216
Pedro Soares <pedrossgarcia88@gmail.com>
2024-12-05 23:08:05 -0300

    intro updated

--- src/components/IntroComp.vue
+++ src/components/IntroComp.vue
@@ -1,7 +1,7 @@
 <template>
    <article id="intro" class="intro-group title">
       <h1>Hi, I am Pedro</h1>
-      <h3>A Front-End Developer</h3>
+      <h3>A Web Developer</h3>
       <span>
          <a class="resume-link" href="https://drive.google.com/file/d/1GTrAQWrrSNAA_Kt6Ej2SfS7scj-RZxze/view?usp=drive_link" target="_blank">Resume</a>
       </span>
